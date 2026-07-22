744859df68e09a115ca0d8e5ae85a02e6a0a2f3d
Vikrant Satheesh Kumar <vikrant1998@gmail.com>
2026-07-21 22:57:24 -0400

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        row, column = len(grid), len(grid[0])
+        from collections import deque
+        q = deque()
+
+        count = 0
+        i = 0
+        while i < row:
+            j = 0
+            while j < column:
+                if grid[i][j] == '1':
+                    count += 1
+                    q.append((i,j))
+                    grid[i][j] = '0'
+
+                while len(q) > 0:
+                    element = q.popleft()
+                    dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+                    for d in dir:
+                        x = element[0] + d[0]
+                        y = element[1] + d[1]
+                        if x >= 0 and x < row and y >= 0 and y < column:
+                            if grid[x][y] == '1':
+                                q.append((x, y))
+                                grid[x][y] = 0
+                j += 1
+            i += 1
+
+        return count
+        
